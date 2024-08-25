73625d29bd3035094281a1c31dd22e405e1cf8c7
fernandofilipuzzi-dev <fernandofilipuzzi.dev@gmail.com>
2024-08-24 23:23:51 -0300

    iniciando repositorio

---
+++ Guia3/Ejercicio2/FormDatosProducto.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Ejercicio2
+{
+    partial class FormDatosProducto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.tbLargo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbTipoProducto = new System.Windows.Forms.ComboBox();
+            this.tbDescripcion = new System.Windows.Forms.TextBox();
+            this.tbPrecioBase = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbTipoMaterial = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(352, 231);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(112, 35);
+            this.btnCancelar.TabIndex = 12;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnOK
+            // 
+            this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOK.Location = new System.Drawing.Point(68, 231);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(112, 35);
+            this.btnOK.TabIndex = 11;
+            this.btnOK.Text = "Aceptar";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // tbLargo
+            // 
+            this.tbLargo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbLargo.Location = new System.Drawing.Point(173, 67);
+            this.tbLargo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbLargo.Name = "tbLargo";
+            this.tbLargo.Size = new System.Drawing.Size(154, 26);
+            this.tbLargo.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(105, 67);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Largo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Tipo de Producto";
+            // 
+            // cbTipoProducto
+            // 
+            this.cbTipoProducto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbTipoProducto.FormattingEnabled = true;
+            this.cbTipoProducto.Items.AddRange(new object[] {
+            "Silla",
+            "Mesa"});
+            this.cbTipoProducto.Location = new System.Drawing.Point(173, 28);
+            this.cbTipoProducto.Name = "cbTipoProducto";
+            this.cbTipoProducto.Size = new System.Drawing.Size(248, 28);
+            this.cbTipoProducto.TabIndex = 13;
+            // 
+            // tbDescripcion
+            // 
+            this.tbDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbDescripcion.Location = new System.Drawing.Point(173, 103);
+            this.tbDescripcion.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbDescripcion.Name = "tbDescripcion";
+            this.tbDescripcion.Size = new System.Drawing.Size(291, 26);
+            this.tbDescripcion.TabIndex = 14;
+            // 
+            // tbPrecioBase
+            // 
+            this.tbPrecioBase.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPrecioBase.Location = new System.Drawing.Point(173, 139);
+            this.tbPrecioBase.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbPrecioBase.Name = "tbPrecioBase";
+            this.tbPrecioBase.Size = new System.Drawing.Size(291, 26);
+            this.tbPrecioBase.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(90, 180);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Material";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(63, 103);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 20);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Descripción";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(102, 139);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 20);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Precio";
+            // 
+            // cbTipoMaterial
+            // 
+            this.cbTipoMaterial.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbTipoMaterial.FormattingEnabled = true;
+            this.cbTipoMaterial.Items.AddRange(new object[] {
+            "Pino",
+            "Algarrobo",
+            "Caoba"});
+            this.cbTipoMaterial.Location = new System.Drawing.Point(173, 180);
+            this.cbTipoMaterial.Name = "cbTipoMaterial";
+            this.cbTipoMaterial.Size = new System.Drawing.Size(248, 28);
+            this.cbTipoMaterial.TabIndex = 19;
+            // 
+            // FormDatosProducto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 293);
+            this.Controls.Add(this.cbTipoMaterial);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbPrecioBase);
+            this.Controls.Add(this.tbDescripcion);
+            this.Controls.Add(this.cbTipoProducto);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.tbLargo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormDatosProducto";
+            this.Text = "Datos del producto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        public System.Windows.Forms.TextBox tbDescripcion;
+        public System.Windows.Forms.TextBox tbLargo;
+        public System.Windows.Forms.ComboBox cbTipoProducto;
+        public System.Windows.Forms.TextBox tbPrecioBase;
+        public System.Windows.Forms.ComboBox cbTipoMaterial;
+    }
+}
---
+++ Guia3/Ejercicio2/FormPrincipal.cs
@@ -0,0 +1,95 @@
+﻿
+using System;
+using System.Windows.Forms;
+using System.Collections;
+using Ejercicio2.Models;
+
+namespace Ejercicio2
+{
+    public partial class FormPrincipal : Form
+    {
+        ArrayList presupuestados = new ArrayList();
+        Presupuesto presupuesto;
+
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnIniciarPresupuesto_Click(object sender, EventArgs e)
+        {
+            FormDatosCliente fCliente = new FormDatosCliente();
+            if (fCliente.ShowDialog() == DialogResult.OK)
+            {
+                string nombre = fCliente.tbNombre.Text;
+                string direccion = fCliente.tbDireccion.Text;
+                presupuesto = new Presupuesto(nombre, direccion);
+                lbCliente.Text = nombre;
+                lbDireccion.Text = direccion;
+
+                #region
+                btnAgregarProducto.Enabled = true;
+                btnVerPresupuesto.Enabled = true;
+                btnIniciarPresupuesto.Enabled = false;
+                #endregion
+            }
+        }
+
+        private void btnAgregarProducto_Click(object sender, EventArgs e)
+        {
+            FormDatosProducto fDatos = new FormDatosProducto();
+            if (fDatos.ShowDialog() == DialogResult.OK)
+            {
+                int tipoProducto = fDatos.cbTipoProducto.SelectedIndex;
+                double precioBase = Convert.ToDouble(fDatos.tbPrecioBase.Text);
+                string descripcion = fDatos.tbDescripcion.Text;
+                int tipoMaterial = fDatos.cbTipoMaterial.SelectedIndex + 1;
+
+                Producto nuevo = null;
+                if (tipoProducto == 0)
+                {
+                    double largo = Convert.ToDouble(fDatos.tbLargo.Text);
+                    nuevo = new Mesa(descripcion, precioBase, tipoMaterial, largo);
+                }
+                else if (tipoProducto == 1)
+                {
+                    nuevo = new Silla(descripcion, precioBase, tipoMaterial);
+                }
+
+                if(nuevo != null)
+                    presupuesto.AgregarProducto(nuevo);
+            }
+            fDatos.Dispose();
+        }
+
+        private void btnVerPresupuesto_Click(object sender, EventArgs e)
+        {
+            FormVer formVer = new FormVer();
+            formVer.tbVer.Clear();
+
+            formVer.tbVer.Text += $@"Cliente: {presupuesto.Cliente}
+Dirección: {presupuesto.Cliente}
+
+Items presupuestados:
+";
+            for (int n = 0; n < presupuesto.CantidadProductos; n++)
+            {
+                Producto producto = presupuesto.VerProducto(n);
+
+                formVer.tbVer.Text += $@"
+{producto.VerDetalle()}";
+            }
+            formVer.tbVer.Text += $@"
+Total:{presupuesto.CostoTotal,25:f2}";
+
+            formVer.ShowDialog();
+            formVer.Dispose();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
