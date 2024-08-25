21b606665745850dabb2f43ee9be701cfa2a311e
fernandofilipuzzi-dev <fernandofilipuzzi.dev@gmail.com>
2024-08-25 12:16:51 -0300

    .

---
+++ Guia3/Ejercicio4_prestadores_medicos/Models/Persona.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Ejercicio4.Models
+{
+    abstract class Persona
+    {
+        public string ApellidosNombre { get; set; }
+
+        public Persona(string apn)
+        {
+            ApellidosNombre = apn;
+        }
+    }
+}
---
+++ Guia3/Ejercicio4_prestadores_medicos/Models/Prestacion.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Ejercicio4.Models
+{
+    abstract class Prestacion : Persona
+    {
+        public double MontoTotal { get; protected set; }
+        public Prestador Medico { get; set; }
+
+        public Prestacion(string apn, Prestador medico)
+            : base(apn)
+        {
+            this.Medico = medico;
+            this.Medico.AgregarConsulta(this);
+        }
+
+        abstract public double PrestarServicio();
+    }
+}
